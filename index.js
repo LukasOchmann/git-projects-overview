3b98addedd3260382fb897ac76d2676464e35d24
Lukas Ochmann <lukas.ochmann@seitenbau.com>
2017-12-14 12:21:43 +0100

    tests

--- index.js
+++ index.js
@@ -13,16 +13,12 @@ function isGitDir(projectPath) {
 
 fs.readdir(path.resolve('Dokumente/Projekt/'), (err, paths) => {
   const dirs = paths
-              .filter((path) => isDir(path))
-              .filter((path) => isGitDir(path))
-              .map((gitDir) => {
-                return simplegit(path.resolve('Dokumente/Projekt/', gitDir))
-              });
-              console.log(dirs[13]);
-              // .map((git) => Object.assign({}, git, {status: git.git.status()}));
-              dirs[13].status((err, status) => {
-                console.log(status);
-              })
+              .filter((path) => isDir(path) && isGitDir(path))
+              .map((gitDir) => simplegit(path.resolve('Dokumente/Projekt/', gitDir)));
+
+              // dirs[14].branch((err, status) => {
+              //   console.log(status);
+              // })
 });
 
 // simplegit().stash(() => {
